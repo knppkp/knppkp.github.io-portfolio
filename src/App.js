66a680329d8fce94b0ba286588d3aa5307d0aebc
knppkp <140078295+knppkp@users.noreply.github.com>
2025-01-13 11:09:51 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
+import ProjectPage from './ProjectPage';
 
 function App() {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/portfolio" element={<LandingPage />} />
+        <Route path="/portfolio/:project" element={<ProjectPage />} />
+      </Routes>
+    </Router>
   );
 }
 
